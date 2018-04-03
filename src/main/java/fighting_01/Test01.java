ab80f53f68cd2dd2dc3365c4f83af8a2f290b422
Your Name <you@example.com>
2018-04-03 14:55:40 +0800

    work02第二次提交

--- src/main/java/fighting_01/Test01.java
+++ src/main/java/fighting_01/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("git练习现在开始。。。。");
 		System.out.println("work01进行第一次修改。。。。");
+		System.out.println("work02进行第二次修改。。。。");
 	}
 }
